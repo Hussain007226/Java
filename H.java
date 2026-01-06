e680fbf42ac52775291682875b4833b47a5be53d
Hussain007226 <hussaintinwala2008@gmail.com>
2026-01-07 01:43:08 +0530

    Add files via upload

---
+++ H.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class H{
+
+
+	public static void main(String[] args){
+
+	System.out.println("Taking Input from the user");
+	Scanner sc = new Scanner(System.in);
+
+	//System.out.println("Enter Number 1");
+	//int a = sc.nextInt();
+	
+	//System.out.println("Enter Number 2");
+	//int b = sc.nextInt();
+	
+	//System.out.println("Addition of numbers given is:" +(a+b));
+	
+	//boolean b1 = sc.hasNextInt();
+	//System.out.println(b1);
+	
+	String str = sc.nextLine();
+	System.out.println(str);
+
+
+	}
+}
