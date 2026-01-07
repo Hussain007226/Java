286b185b6de3a165833573da007e1cd68e11da46
Hussain-Tinwala7869 <hussaintinwala2008@gmail.com>
2026-01-08 00:55:58 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class A{
+	public static void main(String[] args){
+
+		System.out.println("All this so that I can score 100 in JAVA and 95 percent in 4 Sem");
+		System.out.println(" ");
+		System.out.println(" ");
+
+
+
+		// Scanner sc = new Scanner(System.in);
+		// System.out.println("Enter a number to check it is greater then 8 or not");
+		// int a = sc.nextInt();
+		// System.out.println(a>8); 
+
+		int v = 4;
+		int u = 2;
+		int a = 5;
+     		int s = 7;
+
+		double x = (v*v) - (u*u) / (2*a*s);
+		System.out.println(x);
+
+	
+
+
+	}
+
+}
